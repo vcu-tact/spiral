cb3ad91d69c3e08ed2519c3e6550ddf43950de77
yourusername <tact@vcu.edu>
2021-04-08 19:33:03 -0400

    reformatting

--- spiral/homework.py
+++ spiral/homework.py
@@ -2,6 +2,6 @@ def spiralize(number):
 
     x = (number - 1) / 2
 
-    diagonalSum = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
+    diagonal = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
 
-    return diagonalSum
+    return diagonal
