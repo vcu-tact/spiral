8d30aa83fdd64f6916abc21bdce77de364d8730e
yourusername <tact@vcu.edu>
2021-04-08 18:15:13 -0400

    reformatting to pass python package

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,7 +1,7 @@
 def spiralize(number):
     
-    x = (number - 1)/2
-
-    diagonalSum = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
+    x = (number - 1) /2; 
+    
+    diagonalSum = (3 + 2 * x * (8 * x * x + 15 * x + 13))/3
 
     return diagonalSum
