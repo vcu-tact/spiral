ee8908904283ac71d14f06b2c1cab8f7a45eda2f
yourusername <tact@vcu.edu>
2021-04-09 15:58:00 -0400

    moved x out of if and else statement

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,11 +1,13 @@
 def spiralize(number):
 
+    x = (number - 1) / 2
+
     if number == 1:
-        diagonal = 1
+        return_value = 1
     else:    
-        x = (number - 1) / 2
-        diagonal = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
-    return int(diagonal)
+        return_value = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
+    return return_value
+
 
 if __name__ == "__main__":
     print(spiralize(501))
