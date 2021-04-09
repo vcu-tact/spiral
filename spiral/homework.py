081cc27785b2ae38ba15b09796ab93f4fbf60cbd
yourusername <tact@vcu.edu>
2021-04-09 16:05:33 -0400

    created separate main function to call spiralize

--- spiral/homework.py
+++ spiral/homework.py
@@ -2,12 +2,15 @@ def spiralize(number):
 
     x = (number - 1) / 2
 
-    if number == 1:
-        return_value = 1
-    else:    
-        return_value = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
+    n = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
+
+    return_value = int(n)
+
     return return_value
 
 
-if __name__ == "__main__":
-    print(spiralize(501))
+def main():
+
+    dSum = spiralize(501)
+    
+    print(dSum)
