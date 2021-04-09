2aa51ea0ff25f511806d87b320299d7fb6e2ccb7
yourusername <tact@vcu.edu>
2021-04-09 15:32:23 -0400

    made changes to return statement, created two separate statements

--- spiral/homework.py
+++ spiral/homework.py
@@ -3,5 +3,7 @@ def spiralize(number):
     x = (number - 1) / 2
 
     diagonal = (3 + 2 * x * (8 * x * x + 15 * x + 13)) / 3
+    
+    intSum = int(diagonal)
 
-    return int(diagonal)
+    return intSum
